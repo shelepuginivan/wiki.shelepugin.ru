bd15a443eb2da98b4a722bf68902ba860b089068
shelepuginivan <shelepuginivanm@gmail.com>
2025-09-14 04:47:41 +0300

    style: `npm run format`

--- src/components/packaging/index.ts
+++ src/components/packaging/index.ts
@@ -3,4 +3,4 @@ import ArchPkg from './ArchPkg.astro'
 import AurHelper from './AurHelper.astro'
 import Repology from './Repology.astro'
 
-export { ArchPkg, AurHelper, Repology };
+export { ArchPkg, AurHelper, Repology }
